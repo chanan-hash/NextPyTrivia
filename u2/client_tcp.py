fa16b191057e34eabfe3d6d600a0e9a6bb3022f2
DESKTOP-JK2VDE2\חנן <chananhelman@gmail.com>
2023-09-07 21:56:18 +0300

    making client and server talk

--- u2/client_tcp.py
+++ u2/client_tcp.py
@@ -1,3 +1,7 @@
+# This client is based on this videos:
+# https://courses.campus.gov.il/courses/course-v1:CS+GOV_CS_networkpy103+2020_1/courseware/52a3378d07b4483eb4cdb15b899fd472/89de92924e64477fa43baac6a313e540/
+# https://courses.campus.gov.il/courses/course-v1:CS+GOV_CS_networkpy103+2020_1/courseware/52a3378d07b4483eb4cdb15b899fd472/e3da5aa2ac79413cbdced241bdc32c18/?child=first
+
 import socket
 
 # we are creating an object that from socket type,
@@ -9,13 +13,18 @@ my_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 # my_socket.connect((IP,PORT)) ---> it getting an object, and inside the object we'll have the IP adr, and PORT
 my_socket.connect(("127.0.0.1", 8820))  # 127.0.0.1 --> local host
 
-# Sending a String to the server. The socket can transfer only binary data, so we're using the 'encode()' function to
-# convert it to binary sequence
-my_socket.send("hello".encode())
-data = my_socket.recv(1024).decode()  # recv - receive, getting back from the server response,
-                                      # and decode() to convert it from binary to String.
-                                      # recv(1024) --> 1024, is the max byte number we are asking to take out form the socket, we can take less than 1024 bytes, but not more.
-print("The server sent: " + data)
+data = ""
+while data != "Bye": # while loop so the client can send to the server, till it ask to be closed
+    msg = input("Please enter your message\n") # taking an input from the client
+    # Sending a String to the server. The socket can transfer only binary data, so we're using the 'encode()' function to
+    # convert it to binary sequence
+    my_socket.send(msg.encode())
+    data = my_socket.recv(1024).decode()  # recv - receive, getting back from the server response,
+                                          # and decode() to convert it from binary to String.
+                                          # recv(1024) --> 1024, is the max byte number we are asking to take out form the socket, we can take less than 1024 bytes, but not more.
+    print("The server sent: " + data)
+
+print("Closing client socket")
 my_socket.close() # closing the socket
 
 # To check the client side we'll use echo server, it will echo to the client the same data he sent
--- u2/server_tcp.py
+++ u2/server_tcp.py
@@ -1,21 +1,33 @@
+# This server is based on this video
+# https://courses.campus.gov.il/courses/course-v1:CS+GOV_CS_networkpy103+2020_1/courseware/52a3378d07b4483eb4cdb15b899fd472/e3da5aa2ac79413cbdced241bdc32c18/?child=first
+
 from socket import  *
 
 # Creating a new socket object
 server_socket = socket(AF_INET,SOCK_STREAM) # IP protocol, and TCP protocol
-server_socket.bind("0.0.0.0",8820) # connecting between the server we've created, to local IP and PORT, that are found on the server itself
-                                            # getting a tuple with IP and PORT
-                                            # IP - 0.0.0.0, means listen to everyone who addressing to your outside ip or everyone that address you from "local host"
-                                            # listen to every request from inside the computer or outside
+server_socket.bind(("0.0.0.0",8820)) # Taking only One argument.
+                                     # Connecting between the server we've created, to local IP and PORT, that are found on the server itself
+                                     # getting a tuple with IP and PORT
+                                     # IP - 0.0.0.0, means listen to everyone who addressing to your outside ip or everyone that address you from "local host"
+                                     # listen to every request from inside the computer or outside
 
 server_socket.listen() # ready to listen to clients
 print("Server is up and running") # printing to the screen that everything is ready
 (client_socket,client_address) = server_socket.accept() # approving the connection, the method waiting to a request from a client
 print("Client connected") # This line of code will run, only if a client will connect to the server
+                          # accept() --> returns a tuple, with 2 objects: 1. all the data that we need to communicate with the client. 2. IP and PORT that addressed to the server
 
-# accept() --> returns a tuple, with 2 objects: 1. all the data that we need to communicate with the client. 2. IP and PORT that addressed to the server
-
-data = client_socket.recv(1024).decode() # getting the data from the client, and decoding it from bytes to String that we can understand
-print("Client sent: " + data)
-client_socket.send(data.encode()) # sending back to the client a message as we've done in the client side, by encoding it
+while True: # while the client didn't ask to close the socket
+    data = client_socket.recv(1024).decode() # getting the data from the client, and decoding it from bytes to String that we can understand
+    print("Client sent: " + data)
+    client_socket.send(data.encode()) # sending back to the client a message as we've done in the client side, by encoding it
+    if data == "Quit": # condition for closing the socket
+        print("Closing client socket now...")
+        client_socket.send("Bye".encode()) # Then the client will get a "Bey" message and will close the socket according to his condition (waiting for "Bye", look there)
+        break # To jump out of the loop
+    client_socket.send(data.encode())
 
 client_socket.close() # closing the communication with the client on the client's object
+
+# now we have 2 options: 1. going to listen mode and waiting to a new client, 2. or close the server's socket
+server_socket.close()
